2ec1fdfeb892c77f9a43b2df554020bab2b6d21c
audunrug <audunrug@uio.no>
2022-12-02 13:05:41 +0100

    Finished part 2

--- day_2.py
+++ day_2.py
@@ -1,19 +1,9 @@
-#Part 1
-p1 = {"A" : "rock",
-      "B" : "paper",
-      "C" : "scissor"}
+#For part 1
+p1 = {"A" : "rock", "B" : "paper", "C" : "scissor"}
+p2 = {"X" : "rock", "Y" : "paper", "Z" : "scissor"}
 
-p2 = {"X" : "rock",
-      "Y" : "paper",
-      "Z" : "scissor"}
-
-shape_score = {"rock" : 1,
-               "paper" : 2,
-               "scissor" : 3}
-
-result_score = {"lose" : 0,
-                "draw" : 3,
-                "win" : 6}
+shape_score = {"rock" : 1, "paper" : 2, "scissor" : 3}
+result_score = {"lose" : 0, "draw" : 3, "win" : 6}
 
 def game_result_p2(shapes):
     if shapes[0] == shapes[1]:
@@ -34,6 +24,20 @@ def game_result_p2(shapes):
         else:
             return "lose"
 
+#for part 2
+p2_strat = {"X" : "lose",
+            "Y" : "draw",
+            "Z" : "win"}
+
+def choose_shape(strat, p1):
+    if strat ==  "draw":
+        return p1
+    else:
+        for shape in ["rock", "paper", "scissor"]:
+            if game_result_p2([p1, shape]) == strat:
+                return shape
+
+#part 1 solution
 scores = []
 with open("day_2_input.txt", "r") as file:
     for line in file.read().splitlines():
@@ -42,5 +46,17 @@ with open("day_2_input.txt", "r") as file:
         result = game_result_p2(shapes)
         tot_score = result_score[result] + shape_score[shapes[1]]
         scores.append(tot_score)
+print(f"Sum of scores (part 1): {sum(scores)}") #11449
 
-print(f"Sum of scores: {sum(scores)}")
+#part 2 solution
+scores_2 = []
+with open("day_2_input.txt", "r") as file:
+    for line in file.read().splitlines():
+        play = line.split(" ")
+        p1_shape = p1[play[0]]
+        strat = p2_strat[play[1]]
+        p2_shape = choose_shape(strat, p1_shape)
+        #print(p1_shape, p2_shape, strat)
+        tot_score = result_score[strat] + shape_score[p2_shape]
+        scores_2.append(tot_score)
+print(f"Sum of scores (part 2): {sum(scores_2)}") #13187
