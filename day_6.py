f9118d4fb3354d667bba30610b44094529037201
audunrug <audunrug@uio.no>
2022-12-06 14:47:40 +0100

    Solved it!

---
+++ day_6.py
@@ -0,0 +1,18 @@
+def split_by_marker(input_str, l):
+    segments = []
+    last = 0
+    for i in range(len(full_str)-l):
+        chars = set(full_str[i:i+l])
+        if len(chars) == l:
+            segments.append(full_str[last:i+l])
+            last = i
+    return segments
+
+with open("day_6_input.txt", "r") as file:
+    full_str = file.read().strip()
+
+part_1 = split_by_marker(full_str, 4)
+part_2 = split_by_marker(full_str, 14)
+
+print(f"Length of first segment (4 char marker): {len(part_1[0])}")
+print(f"Length of first segment (14 char marker): {len(part_2[0])}")
